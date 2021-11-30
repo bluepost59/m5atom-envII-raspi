49b014afe0aef0e7625a5baec41aba84c1043301
bluepost59 <kaput.kat@gmail.com>
2021-11-30 21:28:48 +0900

    first commit

---
+++ atom_envII.ino
@@ -0,0 +1,14 @@
+#include <M5Atom.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+  M5.begin(true, false, true);
+  Serial.println("hello");
+}
+
+void loop()
+{
+  Serial.println("loop");
+  // put your main code here, to run repeatedly:
+}
